7f60419bd00c3c3eab12a07518f79b6bad116057
Asger Nohns <asger@nohns.dk>
2023-03-05 04:21:49 +0100

    feat: sqlite implementation of event store

---
+++ servers/pkg/sqlite/schema.go
@@ -0,0 +1,22 @@
+package sqlite
+
+import "database/sql"
+
+func Migrate(db *sql.DB) error {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS events (
+			aggregate_id TEXT,
+			version INTEGER NOT NULL UNIQUE,
+			aggregate_version INTEGER,
+			type TEXT NOT NULL,
+			at DATETIME DEFAULT CURRENT_TIMESTAMP,
+			data BLOB NOT NULL
+		);
+		CREATE INDEX IF NOT EXISTS events_aggregate_id_version ON events (aggregate_id, version);
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
