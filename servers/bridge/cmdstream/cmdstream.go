853bdf294faac046072469bd441d7f4c5b6c3e31
Asger Nohns <asger@nohns.dk>
2023-03-05 19:36:50 +0100

    refactor(cmdstream): split bridge proto

--- servers/bridge/cmdstream/cmdstream.go
+++ servers/bridge/cmdstream/cmdstream.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"log"
 
-	"github.com/nohns/semesterprojekt2/proto/gen/go/cloud/bridge/v1"
+	bridgepb "github.com/nohns/semesterprojekt2/proto/gen/go/cloud/bridge/v1"
 )
 
 type stream struct {
-	c       bridge.BridgeServiceClient
-	d       *distributor
-	answers chan bridge.StreamAnswer
+	c bridgepb.CmdServiceClient
+	d *distributor
 }
 
-func New(c bridge.BridgeServiceClient, d *distributor) *stream {
+func New(c bridgepb.CmdServiceClient, d *distributor) *stream {
 	return &stream{
 		c: c,
 		d: d,
@@ -22,28 +21,17 @@ func New(c bridge.BridgeServiceClient, d *distributor) *stream {
 
 // Starts new stream of commands. This function blocks until the stream is closed.
 func (s *stream) Listen(ctx context.Context) error {
-	sc, err := s.c.StreamActions(ctx)
+	sc, err := s.c.StreamCommands(ctx, &bridgepb.StreamCommandsRequest{})
 	if err != nil {
 		return err
 	}
 
-	// Send answers in a separate goroutine, and receive in this one.
-	go s.send(sc.Context(), sc)
+	// Receive in this one and block until a potential error occurs
 	return s.recv(sc.Context(), sc)
 }
 
-// Receives answers from the bridge and sends them to the cloud
-func (s *stream) send(ctx context.Context, sc bridge.BridgeService_StreamActionsClient) error {
-	for answer := range s.answers {
-		if err := sc.Send(&answer); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // Receives commands from the cloud and distributes them to the correct services
-func (s *stream) recv(ctx context.Context, sc bridge.BridgeService_StreamActionsClient) error {
+func (s *stream) recv(ctx context.Context, sc bridgepb.CmdService_StreamCommandsClient) error {
 	for {
 		streamcmd, err := sc.Recv()
 		if err != nil {
