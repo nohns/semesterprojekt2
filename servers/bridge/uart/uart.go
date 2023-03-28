53ee6902aababfd5e6f6088a096fa70e963b433c
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-03-28 20:29:32 +0200

    Feat: uart 1st implementation not done yet

--- servers/bridge/uart/uart.go
+++ servers/bridge/uart/uart.go
@@ -2,40 +2,101 @@ package uart
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"go.bug.st/serial"
 )
 
-func main() {
-    // Open the serial port
-    port, err := serial.Open("/dev/ttyUSB0", &serial.Mode{
-        BaudRate: 115200,
-        DataBits: 8,
-        StopBits: serial.OneStopBit,
-        Parity:   serial.NoParity,
-    })
-    if err != nil {
-        panic(err)
-    }
-    defer port.Close()
+const USB = "/dev/tty.usbmodem14501"
 
-    // Write some data to the UART device
-    data := []byte("Hello, UART!")
-    n, err := port.Write(data)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("Wrote %d bytes to the UART device.\n", n)
+func Uart() {
+	// Open the serial port
+	port, err := serial.Open("/dev/tty.usbmodem14501", &serial.Mode{
+		BaudRate: 9600,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+		Parity:   serial.NoParity,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer port.Close()
 
-    // Read some data from the UART device
-    buffer := make([]byte, 100)
-    n, err = port.Read(buffer)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("Read %d bytes from the UART device: %s\n", n, string(buffer[:n]))
+	// Write some data to the UART device
+	data := []byte("b")
+	n, err := port.Write(data)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Wrote %d bytes to the UART device.\n", n)
 
-    // Wait for a second
-    time.Sleep(time.Second)
+	// Read some data from the UART device
+	buffer := make([]byte, 100)
+	n, err = port.Read(buffer)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Read %d bytes from the UART device: %s\n", n, string(buffer[:n]))
+
+	// Wait for a second
+	//Loop forever
+	for {
+		data := []byte("b")
+		n, err := port.Write(data)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Wrote %d bytes to the UART device.\n", n)
+		buffer := make([]byte, 100)
+		n, err = port.Read(buffer)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Read %d bytes from the UART device: %s\n", n, string(buffer[:n]))
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+//I think I want to have a seperate goroutine running as reader
+// and I should have a goroutine running as writer
+
+//Real abstraction layer for the UART
+
+type uart interface {
+	read() (byte, error)
+	write(byte) (byte, error)
+}
+
+type uartImpl struct {
+	port serial.Port
+}
+
+func New() *uartImpl {
+	return &uartImpl{}
+}
+
+func (u *uartImpl) Read() (byte, error) {
+	// Read some data from the UART device
+	buffer := make([]byte, 100)
+	n, err := u.port.Read(buffer)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	fmt.Printf("Read %d bytes from the UART device: %s\n", n, string(buffer[:n]))
+	return buffer[0], nil
+}
+
+func (u *uartImpl) Write(b byte) (byte, error) {
+	// Write some data to the UART device
+	data := []byte{b}
+	n, err := u.port.Write(data)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	fmt.Printf("Wrote %d bytes to the UART device.\n", n)
+	return data[0], nil
 }
