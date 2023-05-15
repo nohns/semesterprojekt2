2a4e2e08069c99cc5e0c090b8c36d923ab54cf35
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-05-15 08:30:48 +0200

    feat: bridge cloud pairing domain logic

--- servers/bridge/domain/domain.go
+++ servers/bridge/domain/domain.go
@@ -4,12 +4,18 @@ import "context"
 
 type domain struct {
 	uart uart
+	cs   certificateStore
 }
 
 type uart interface {
 	AwaitResponse(ctx context.Context, cmd int) ([]byte, error)
 }
 
-func New(uart uart) *domain {
-	return &domain{uart: uart}
+type certificateStore interface {
+	InsertCertificate(id string, certificate []byte, privatekey []byte) error
+	GetCertificate(id string) ([]byte, []byte, error)
+}
+
+func New(uart uart, cs certificateStore) *domain {
+	return &domain{uart: uart, cs: cs}
 }
