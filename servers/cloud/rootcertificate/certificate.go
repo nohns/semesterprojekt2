aebd7323a1aa71ae8afb325a89eff96b3f9d071c
Martin Vad <90182227+dietzy1@users.noreply.github.com>
2023-05-15 08:28:16 +0200

    feat: certificate lib needs cleanup

--- servers/cloud/rootcertificate/certificate.go
+++ servers/cloud/rootcertificate/certificate.go
@@ -13,6 +13,21 @@ import (
 	"time"
 )
 
+//mutual TLS
+
+//CLOUD is ROOT
+//bridge is INTERMEDIATE
+//phone is END
+
+//What needs to happen is we create this certificate on the cloud and then we should look to store it into the database
+//We can then use this certificate to sign the CSR that is sent from the bridge
+//The bridge should then store the certificate into the database and use it to sign the CSR that is sent from the phone
+
+//That means a circle of trust has been established between the cloud, bridge, and phone
+//because the cloud is root and it signs the bridge and then bridge is allowed to sign the phone
+//this means the phone is able to communicate with the cloud because it trusts the bridge and the bridge trusts the cloud
+//Even though the phone does not trust the cloud, it trusts the bridge and the bridge trusts the cloud
+
 func certsetup() (serverTLSConf *tls.Config, clientTLSConf *tls.Config, err error) {
 	// set up our CA certificate
 	ca := &x509.Certificate{
@@ -32,8 +47,6 @@ func certsetup() (serverTLSConf *tls.Config, clientTLSConf *tls.Config, err erro
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
 		BasicConstraintsValid: true,
 	}
-
-	
 
 	// create our private and public key
 	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
